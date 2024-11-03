dda738a97ac715002cb650dcfafe4bd90fbf9ebb
ingalejay1 <ingalejay1@gmail.com>
2024-11-03 11:53:47 +0530

    add

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const TASK_API_END_POINT="http://localhost:8000/api/v1/task";
+export const USER_API_END_POINT="https://task-application-cuvvete.onrender.com/api/v1/user";
+export const TASK_API_END_POINT="https://task-application-cuvvete.onrender.com/api/v1/task";
