ac38f20dc7c3247e62b96c4c9846652a2d42a310
ingalejay1 <ingalejay1@gmail.com>
2024-11-05 10:16:48 +0530

    changes done

--- Frontend/src/components/Popup/Logout.jsx
+++ Frontend/src/components/Popup/Logout.jsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import { USER_API_END_POINT } from '../../utils/constant';
+import React from "react";
+import { USER_API_END_POINT } from "../../utils/constant";
 import { toast } from "react-toastify";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import axios from "axios";
-import '../Styles/LogoutPopup.css';
-import { useNavigate } from 'react-router-dom';
+import "../Styles/LogoutPopup.css";
+import { useNavigate } from "react-router-dom";
 
-const Logout = ({ setShowLogoutPopup, setUser }) => {
-    const navigate = useNavigate();
+const Logout = ({ setShowLogout, setUser }) => {
+  const navigate = useNavigate();
 
-    const logoutHandler = async () => {
-        try {
-            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, {
+        withCredentials: true,
+      });
 
-            if (res.data.success) {
-                setUser(null);
-                localStorage.removeItem('user');
-                localStorage.removeItem('token');
-                navigate("/");
-                toast.success(res.data.message);
-                console.log("Logged out successfully.");
-                setShowLogout(false);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        }
+      if (res.data.success) {
+        setUser(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        navigate("/");
+        toast.success(res.data.message);
+        console.log("Logged out successfully.");
+        setShowLogout(false);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
     }
+  };
 
   return (
-    <div className='logoutPopupCon'>
-        <div className='logoutPopup'>
-            <p>Are you sure you want to Logout?</p>
-            <button className='logoutPopupBtn' onClick={logoutHandler}>Yes, Logout</button>
-            <button className='hideLogoutBtn' onClick={() => setShowLogout(false)}>Cancel</button>
-        </div>
-        <ToastContainer />
+    <div className="logoutPopupCon">
+      <div className="logoutPopup">
+        <p>Are you sure you want to Logout?</p>
+        <button className="logoutPopupBtn" onClick={logoutHandler}>
+          Yes, Logout
+        </button>
+        <button className="hideLogoutBtn" onClick={() => setShowLogout(false)}>
+          Cancel
+        </button>
+      </div>
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 export default Logout;
