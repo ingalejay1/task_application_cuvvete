9dd09697e6b1b3070e292fcbd51307edcdd9e064
ingalejay1 <ingalejay1@gmail.com>
2024-11-05 07:16:46 +0530

    changes done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://task-application-cuvvete.onrender.com', // Update with your backend Render URL in production
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
